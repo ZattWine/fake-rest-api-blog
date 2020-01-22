22818716ad6a671aee48943b84d029a44392a27a
Zayar Tun <12999232+ZattWine@users.noreply.github.com>
2020-01-22 13:24:13 +0630

    Create db.js

---
+++ db.js
@@ -0,0 +1,38 @@
+{
+  "posts": [
+    {
+      "id": 1,
+      "title": "First article",
+      "urlToImage" : "https://scatterjar.com/wp-content/uploads/2016/05/www.scatterjar.com-7061-1024x681.jpg",
+      "description" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "category" : "food",
+      "tag" : "food,health",
+      "publishedAt" : 1579675996398
+    },
+    {
+      "id": 2,
+      "title": "Second article",
+      "urlToImage" : "https://scatterjar.com/wp-content/uploads/2016/05/www.scatterjar.com-7061-1024x681.jpg",
+      "description" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "category" : "food",
+      "tag" : "food,health",
+      "publishedAt" : 1579675996398
+    },
+    {
+      "id": 3,
+      "title": "Third article",
+      "urlToImage" : "https://scatterjar.com/wp-content/uploads/2016/05/www.scatterjar.com-7061-1024x681.jpg",
+      "description" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "category" : "food",
+      "tag" : "food,health",
+      "publishedAt" : 1579675996398
+    }
+  ],
+  "author": {
+    "id" : 1,
+    "name": "anonymous"
+  }
+}
